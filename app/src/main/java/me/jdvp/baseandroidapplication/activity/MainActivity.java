797d9e1bba6f5817d65846a0e3c62b3f443a09e9
JD Porterfield <jdvporterfield@gmail.com>
2019-08-18 18:34:28 -0400

    No Ticket : Migrate to AndroidX

--- app/src/main/java/me/jdvp/baseandroidapplication/activity/MainActivity.java
+++ app/src/main/java/me/jdvp/baseandroidapplication/activity/MainActivity.java
@@ -1,15 +1,11 @@
 package me.jdvp.baseandroidapplication.activity;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
-import butterknife.BindView;
 import dagger.android.support.DaggerAppCompatActivity;
 import me.jdvp.baseandroidapplication.R;
 
 public class MainActivity extends DaggerAppCompatActivity {
-    @BindView(R.id.fragment_container)
-    LinearLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
